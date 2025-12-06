be8915631ce3e53088911a9af3b44ae9a844ebd1
Mohammed Sherbini <sherbinimohd@gmail.com>
2025-12-06 21:13:55 +0200

    b7awel

--- app.py
+++ app.py
@@ -353,5 +353,4 @@ def password_reset_new():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    # If you use Docker, keep host="0.0.0.0"
     app.run(host="0.0.0.0", port=5000, debug=True)
